32d7cd25a417f745bb78bbd7cd3fd839fddd12e0
pandaman404 <valenzuela.ahumada4@gmail.com>
2021-11-17 12:19:53 -0300

    routing fixed

--- website/src/app.js
+++ website/src/app.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/rest-countries-api">
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/country/:code" element={<Country />} />
